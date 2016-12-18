a408f29b544e3118cc14a9f17ab41b9b600229b1
Ahmed Mohaisen <ahm.mu@outlook.com>
2016-12-18 19:00:00 +0300

    dramatic change

--- spreadshirts/js/main.js
+++ spreadshirts/js/main.js
@@ -1,28 +1,48 @@
 $(document).ready(function() {
-  // best selling carousel
+
+
+ // best selling carousel
   $("#best_selling").owlCarousel({
-    autoPlay: false,
-    items: 4,
-    // itemsDesktop : [1199,3],
-    itemsDesktop: [991, 3],
-    itemsDesktopSmall: [880, 2],
-    itemsMobile: [590, 1]
+ 
+      autoPlay: false,
+ 
+      items : 4,
+      // itemsDesktop : [1199,3],
+      itemsDesktop : [991,3],
+      itemsDesktopSmall : [880,2],
+      itemsMobile : [590,1]
+ 
   });
+
+
   $(document).ready(function() {
-    var owl = $("#best_selling");
-    owl.owlCarousel();
-    // Custom Navigation Events
-    $(".next").click(function() {
-      owl.trigger('owl.next');
-    })
-    $(".prev").click(function() {
-      owl.trigger('owl.prev');
-    })
-  });
+ 
+  var owl = $("#best_selling");
+ 
+  owl.owlCarousel();
+ 
+  // Custom Navigation Events
+  $(".next").click(function(){
+    owl.trigger('owl.next');
+  })
+  $(".prev").click(function(){
+    owl.trigger('owl.prev');
+  })
+ 
+});
+
+
   // cutomer saying carousel
   $("#cutomer_saying").owlCarousel({
-    autoPlay: 5000, //Set AutoPlay to 5 seconds
-    singleItem: true,
-    transitionStyle: "fadeUp"
+ 
+      autoPlay: 5000, //Set AutoPlay to 5 seconds
+ 
+      singleItem: true,
+      transitionStyle : "fadeUp"
+ 
   });
+
+
+
+ 
 });
